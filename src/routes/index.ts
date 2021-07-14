66416a13895b06580930a25321986f3182a88d0c
Aswin Sahlan Himawan <ashymee.dev@gmail.com>
2021-07-15 04:51:04 +0700

    ..

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,13 @@
 import express from 'express'
-import notesRouter from './notesRouter'
+// import notesRouter from './notesRouter'
 import studentsRouter from './studentsRouter'
-import usersRouter from './usersRouter'
+// import usersRouter from './usersRouter'
 
 const appRouter = express.Router()
 
 appRouter
-  .use('/users', usersRouter)
+  // .use('/users', usersRouter)
   .use('/students', studentsRouter)
-  .use('/notes', notesRouter)
+// .use('/notes', notesRouter)
 
 export default appRouter
