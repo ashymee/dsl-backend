29fd0d804f657466d96102e983f61c98069d9aab
Aswin Sahlan Himawan <ashymee.dev@gmail.com>
2021-07-15 04:58:12 +0700

    ... --build

--- build/index.js
+++ build/index.js
@@ -22,15 +22,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var body_parser_1 = __importDefault(require("body-parser"));
-var cors_1 = __importDefault(require("cors"));
 var dotenv_1 = __importDefault(require("dotenv"));
 var express_1 = __importDefault(require("express"));
 var config = __importStar(require("./config"));
-var routes_1 = __importDefault(require("./routes"));
 dotenv_1.default.config();
 var app = express_1.default();
-app.use(express_1.default.json()).use(cors_1.default()).use(body_parser_1.default.json()).use('/api', routes_1.default);
 app.get('/', function (req, res) {
     res.render('Welcome..');
 });
