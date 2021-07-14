dc5747bbfbf390c32bce93f300711551fe29ddc9
Aswin Sahlan Himawan <ashymee.dev@gmail.com>
2021-07-15 05:15:18 +0700

    ..

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
-// import bodyParser from 'body-parser'
 import cors from 'cors'
-import dotenv from 'dotenv'
 import express from 'express'
-// import appRouter from './routes'
-
-dotenv.config()
 
 const app = express()
 
 app.use(express.json()).use(cors())
-// .use(bodyParser.json()).use('/api', appRouter)
 
 app.get('/', (req: express.Request, res: express.Response) => {
-  res.render('Welcome..')
+  res.json('Welcome..')
 })
 
 const port = process.env.PORT || 9999
