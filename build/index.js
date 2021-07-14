068108fc098782ea69b3b0f50e8285410b6aabb4
Aswin Sahlan Himawan <ashymee.dev@gmail.com>
2021-07-15 05:12:00 +0700

    rebuild

--- build/index.js
+++ build/index.js
@@ -1,35 +1,17 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+var cors_1 = __importDefault(require("cors"));
 var dotenv_1 = __importDefault(require("dotenv"));
 var express_1 = __importDefault(require("express"));
-var config = __importStar(require("./config"));
 dotenv_1.default.config();
 var app = express_1.default();
+app.use(express_1.default.json()).use(cors_1.default());
 app.get('/', function (req, res) {
     res.render('Welcome..');
 });
 var port = process.env.PORT || 9999;
-app.listen(port, function () { return console.info("server is running on " + config.global.url + ":" + port); });
+app.listen(port, function () { return console.info("server is running on post: " + port); });
 //# sourceMappingURL=index.js.map
