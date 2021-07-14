1037b17c7ddba3e5c1a9b85982e7c094c4676c30
Aswin Sahlan Himawan <ashymee.dev@gmail.com>
2021-07-15 05:15:59 +0700

    ..

--- build/index.js
+++ build/index.js
@@ -4,13 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var cors_1 = __importDefault(require("cors"));
-var dotenv_1 = __importDefault(require("dotenv"));
 var express_1 = __importDefault(require("express"));
-dotenv_1.default.config();
 var app = express_1.default();
 app.use(express_1.default.json()).use(cors_1.default());
 app.get('/', function (req, res) {
-    res.render('Welcome..');
+    res.json('Welcome..');
 });
 var port = process.env.PORT || 9999;
 app.listen(port, function () { return console.info("server is running on post: " + port); });
