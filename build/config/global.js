5b5425c5ef02df4e7e72f76c794959ec2dcfcb69
Aswin Sahlan Himawan <ashymee.dev@gmail.com>
2021-07-15 04:43:28 +0700

    remove unneeded assert -- build

--- build/config/global.js
+++ build/config/global.js
@@ -4,14 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.firebaseAdmin = exports.collections = exports.firestore = exports.db = exports.global = void 0;
-var assert_1 = __importDefault(require("assert"));
 var dotenv_1 = __importDefault(require("dotenv"));
 var firebase_1 = __importDefault(require("firebase"));
 var firebase_admin_1 = __importDefault(require("firebase-admin"));
 var serviceAccountKey_json_1 = __importDefault(require("./serviceAccountKey.json"));
 dotenv_1.default.config();
 var _a = process.env, HOST = _a.HOST, HOST_URL = _a.HOST_URL, FIREBASE_API_KEY = _a.FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN = _a.FIREBASE_AUTH_DOMAIN, FIREBASE_DB_URL = _a.FIREBASE_DB_URL, FIREBASE_PROJECT_ID = _a.FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET = _a.FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID = _a.FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID = _a.FIREBASE_APP_ID, FIREBASE_MEASUREMENT_ID = _a.FIREBASE_MEASUREMENT_ID;
-assert_1.default(HOST, 'host is required');
 exports.global = {
     host: HOST,
     url: HOST_URL,
