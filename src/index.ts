dbff23dc0baba514af5facf16d36a47fb4ea1537
Aswin Sahlan Himawan <ashymee.dev@gmail.com>
2021-07-15 06:00:18 +0700

    fix errors

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
+import path from 'path'
 import * as config from './config'
 import appRouter from './routes'
 
@@ -11,10 +12,14 @@ const app = express()
 
 app
   .use(express.json())
+  .use('/', express.static(path.join(__dirname, 'public')))
   .use(cors())
   .use(bodyParser.json())
-  .use('/', express.static('public'))
   .use('/api', appRouter)
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('<h1>Welcome..</h1>')
+})
 
 const port = process.env.PORT || 9999
 app.listen(port, () => console.info(`server is running on ${config.global.url} :${port}`))
